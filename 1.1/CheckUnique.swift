d5b3d470d780632ce3c9d0193f7f0b2378cc4f25
forzaster <forzaster@gmail.com>
2017-02-05 23:04:41 +0900

    [refactor] 1.1

--- 1.1/CheckUnique.swift
+++ 1.1/CheckUnique.swift
@@ -1,3 +1,25 @@
+// Use Hash map(Dictionary)
+func checkUnique_withDictionary(str: String?) -> Bool {
+  guard let target = str else {
+    return false
+  }
+
+  var dict: Dictionary<String, Int> = [:]
+  let cnt = target.unicodeScalars.count
+  for i in 0 ..< cnt {
+    if let i_ = target.index(target.startIndex, offsetBy: i, limitedBy: target.endIndex) {
+      let ci = String(target[i_])
+      if let _ = dict[ci] {
+        return true
+      }
+      dict[ci] = 1
+    }
+  }
+  return false
+}
+
+
+// Not use Hash map
 func checkUnique(str: String?) -> Bool {
   guard let target = str else {
     return false
@@ -20,29 +42,15 @@ func checkUnique(str: String?) -> Bool {
   return false
 }
 
-func checkUnique2(str: String?) -> Bool {
-  guard let target = str else {
-    return false
-  }
-
-  var dict: Dictionary<String, Int> = [:]
-  let cnt = target.unicodeScalars.count
-  for i in 0 ..< cnt {
-    if let i_ = target.index(target.startIndex, offsetBy: i, limitedBy: target.endIndex) {
-      let ci = String(target[i_])
-      if let _ = dict[ci] {
-        return true
-      }
-      dict[ci] = 1
-    }
-  }
-  return false
-}
-
 print("Input>", terminator: "")
 let line: String? = readLine()
-//let result = checkUnique(str: line)
-let result = checkUnique2(str: line)
+var result = checkUnique_withDictionary(str: line)
+if result {
+  print("No Unique")
+} else {
+  print("Unique")
+}
+result = checkUnique(str: line)
 if result {
   print("No Unique")
 } else {
