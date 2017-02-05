608abd06f484f5bef1e4bd50d274f3bcd0844d91
forzaster <forzaster@gmail.com>
2017-02-05 23:00:51 +0900

    [update] Added another method for checkUnique

--- 1.1/CheckUnique.swift
+++ 1.1/CheckUnique.swift
@@ -20,9 +20,29 @@ func checkUnique(str: String?) -> Bool {
   return false
 }
 
+func checkUnique2(str: String?) -> Bool {
+  guard let target = str else {
+    return false
+  }
+
+  var dict: Dictionary<String, Int> = [:]
+  let cnt = target.unicodeScalars.count
+  for i in 0 ..< cnt {
+    if let i_ = target.index(target.startIndex, offsetBy: i, limitedBy: target.endIndex) {
+      let ci = String(target[i_])
+      if let _ = dict[ci] {
+        return true
+      }
+      dict[ci] = 1
+    }
+  }
+  return false
+}
+
 print("Input>", terminator: "")
 let line: String? = readLine()
-let result = checkUnique(str: line)
+//let result = checkUnique(str: line)
+let result = checkUnique2(str: line)
 if result {
   print("No Unique")
 } else {
