66cbf72b8bc7c357567e9e5a937250f96c9f5959
forzaster <forzaster@gmail.com>
2017-02-11 21:53:08 +0900

    [add] 2.2

---
+++ 2.2/main.swift
@@ -0,0 +1,27 @@
+func findKthItem(item: Item, k: Int) -> Item? {
+  var idx: Int = 1
+  var ite: Item? = item
+  var ret: Item? = item
+  while ite?.next() != nil {
+    if (idx < k) {
+      idx += 1
+    } else {
+      ret = ret!.next()
+    }
+    ite = ite!.next()
+  }
+  return ret
+}
+
+
+let item: Item? = Item.loadFromStdIn()
+if item != nil {
+  print("Input k>", terminator:"")
+  let inStr = readLine()
+  if inStr != nil {
+    let k: Int = Int(inStr!)!
+    print (String(k) + "th item>>")
+    let ret = findKthItem(item: item!, k: k)
+    ret?.dump()
+  }
+}
