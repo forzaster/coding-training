8ed24eee44df8f2259b0618f728d073e723efc1f
forzaster <forzaster@gmail.com>
2017-02-13 22:35:43 +0900

    [update] 2.5

--- 2.5/main.swift
+++ 2.5/main.swift
@@ -10,10 +10,41 @@ func toValue(item: Item) -> Int {
   return ret
 }
 
+func fromValue(value: Int) -> Item? {
+  var digit: Int = 1
+  var tmp: Int = 1
+  while true {
+    tmp *= 10
+    if tmp > value {
+      break
+    }
+    digit += 1
+  }
+
+  var v: Int = value
+  var ret: Item? = nil
+  var prev: Item? = nil
+  for _ in 0 ..< digit {
+    let item = Item(v % 10)
+    if ret == nil {
+      ret = item
+    }
+    if prev != nil {
+      let _ = prev!.setNext(item)
+    }
+    prev = item
+    v /= 10
+  }
+
+  return ret
+}
+
 let lhs = Item.loadFromStdIn()
 let rhs = Item.loadFromStdIn()
 
 let l: Int = toValue(item: lhs)
 let r: Int = toValue(item: rhs)
-let ret: Int = l + r
-print(String(l) + " + " + String(r) + " = " + String(ret))
+let value: Int = l + r
+print(String(l) + " + " + String(r) + " = " + String(value))
+let item = fromValue(value: value)
+Item.dumpList(item!)
